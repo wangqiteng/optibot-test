0e9b5f7da72357f21a9c155b5e2b5db964f2aef0
qiteng.wang <qiteng.wang@dbappsecurity.com.cn>
2025-06-27 17:28:44 +0800

    写一些有问题的代码，以便测试,用于测试

--- Test.java
+++ Test.java
@@ -1,5 +1,20 @@
 public class Test {
     public static void main(String[] args) {
+        // 故意写一些有问题的代码，以便测试
+        int  i = 1/0;
         System.out.println("Hello World!");
+
+        Integer a = 129;
+        Integer b = 129;
+        System.out.println(a == b);
+
+
+        Integer c = 1;
+        Integer d = 1;
+        System.out.println(c == d);
+
+        int[] arr = new int[10];
+        System.out.println(arr[10]);
+
     }
 }
